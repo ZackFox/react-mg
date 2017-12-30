b442d65a4d8e355818a2194bde6f4b64b68eeb28
Segey <firstthinker1989@gmail.com>
2017-12-31 03:42:27 +0500

    generation couples fix

--- src/utils/generateCouples.js
+++ src/utils/generateCouples.js
@@ -2,9 +2,16 @@ import shuffle from './shuffle';
 
 const generateCouples = array => {
   const random = shuffle(array).slice(0, 9);
-  const couples = random.concat(random.map(obj => ({ ...obj })));
-  couples.forEach((item, i) => (item.id = i));
-  return shuffle(couples);
+  const couples = random.concat(random);
+  const result = couples.map((item, i) => ({
+    id: i,
+    value: item,
+    isFlipped: true,
+    IsMatch: false,
+  }));
+
+  console.log(result);
+  return shuffle(result);
 };
 
 export default generateCouples;
